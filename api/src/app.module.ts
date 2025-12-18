90fe0c8941463a30abff132b92ac600d81ea9ab7
Frederic Mas <fredfredfredm@yahoo.fr>
2025-12-18 11:59:17 +0100

    Stack: backend database connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './shared/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // only for starting devs
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
