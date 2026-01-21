8f4b317a5b5e51e63963b4bf4e7ca43da6554375
Frederic Mas <fredfredfredm@yahoo.fr>
2026-01-21 15:56:52 +0100

    Stack: remove unusued history development

--- modules/execution-log/dto/update-execution-log.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateExecutionLogDto } from './create-execution-log.dto';
-
-export class UpdateExecutionLogDto extends PartialType(CreateExecutionLogDto) {
-  id: number;
-}
--- modules/execution-log/execution-log.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ExecutionLogService } from './execution-log.service';
-import { ExecutionLogGateway } from './execution-log.gateway';
-
-@Module({
-  providers: [ExecutionLogGateway, ExecutionLogService],
-})
-export class ExecutionLogModule {}
